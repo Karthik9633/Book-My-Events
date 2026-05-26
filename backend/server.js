9343240f6db1a332f32fe28d44973fb389c39165
Karthik <karthikbalu5533@gmail.com>
2026-05-26 14:44:57 +0530

    server.js was updated for deploying

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true, }));
+app.use(cors({ origin: ["http://localhost:5173","https://book-my-events-tau.vercel.app/"], credentials: true, }));
 
 app.use(express.json());
 
